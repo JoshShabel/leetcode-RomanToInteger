b87616310980bc9a0779e932ac47a3150f759c39
Bestest <joshshabel@gmail.com>
2022-05-17 19:30:34 -0700

    Added class for case V

--- Solution.java
+++ Solution.java
@@ -1,32 +1,32 @@
 class Solution {
+
     public int romanToInt(String s) {
-        switch(s.substring(0, 1){
+        switch (s.substring(0, 1)) {
             case "I":
-        return I(s);
+                return I(s);
             case "V":
-            return V(s);
+                return V(s);
             case "X":
                 return X(s);
             case "L":
-            return L(s);
+                return L(s);
             case "C":
-            return C(s);
+                return C(s);
             case "D":
-            return D(s);
+                return D(s);
             case "M":
-            return M(s);
+                return M(s);
         }
-               
     }
-    
-    public int I(String s)
-               if (s.length() == 1)
-               return 1;
-               else if (s.substring(1, 2).equals("I"))
-               return 1 + I(s.substring(1, s.length() - 1));
-               else
-               if (s.substring(1, 2).equals("V")){
-               if    
-               }
-               
+
+    public int I(String s) {
+        if (s.length() == 1) return 1; else if (s.substring(1, 2).equals("I")) return 1 + I(s.substring(1, s.length() - 1)); else if (s.substring(1, 2).equals("V")) {
+            if (s.length() == 2) return 4; else return 9;
+        }
+    }
+
+    public int V(String s) {
+        if (s.length() == 1) return 5; else if (s.substring(1, 2).equals("V")) return 5 + V(s.substring(1, s.length() - 1)); else return 5 + I(s.substring(1, s.length() - 1));
+    }
 }
+
